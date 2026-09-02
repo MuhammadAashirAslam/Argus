98bf6fd54a8fefcc8a731c70e92ce6a778bb8fc8
Muhammad Aashir Aslam <aashiraslam716@gmail.com>
2026-09-02 12:11:19 +0500

    test(benchmarks): add unit tests for benchmark cases and comparative trial runner

---
+++ benchmarks/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./types.js";
+export * from "./cases/dataset.js";
+export * from "./baselines/baseline_a.js";
+export * from "./baselines/baseline_b.js";
+export * from "./runner.js";
